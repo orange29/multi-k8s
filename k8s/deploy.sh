8a0b5f130495bd1741c03b4dca23f957d97a97dd
gmiles <gregory.miles@polarisalpha.com>
2020-06-15 15:58:36 -0600

    added deployment scripts

---
+++ k8s/deploy.sh
@@ -0,0 +1,16 @@
+docker build -t orange29/multi-client:latest -t orange29/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t orange29/multi-server:latest -t orange29/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t orange29/multi-worker:latest -t orange29/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push orange29/multi-client:latest
+docker push orange29/multi-server:latest
+docker push orange29/multi-worker:latest
+
+docker push orange29/multi-client:$SHA
+docker push orange29/multi-server:$SHA
+docker push orange29/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=orange29/multi-server:$SHA
+kubectl set image deployments/client-deployment client=orange29/multi-client:$SHA
+kubectl set image deployments/worker worker=worker=orange29/multi-worker:$SHA
